0fd100000d8ba663507c53b342759ac8960b6f22
Mohamed El-Alkamy <136917196+mohamed-alkamy@users.noreply.github.com>
2025-09-03 11:58:06 -0700

    Delete check-char-type.py

--- check-char-type.py
+++
@@ -1,9 +0,0 @@
-print('Character type checker')
-char = input('Enter a character here: ')
-
-if char.isalpha():
-    print('This is a letter')
-elif char.isdigit():
-    print('This is a number')
-else:
-    print('This is a special character.')
